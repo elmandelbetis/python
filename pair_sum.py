d323add0d3410293a56d0629743d7d4c93c2b18b
Álvaro Maldonado Medina <126078785+elmandelbetis@users.noreply.github.com>
2025-06-27 01:19:13 +0200

    more stuff

--- pair_sum.py
+++ pair_sum.py
@@ -1,5 +1,20 @@
 # -*- coding: utf-8 -*-
 """
 name: pair_sum
+
+Sums up all the even numbers from 1 to 100
 """
 
+def is_even(number):
+    if number % 2 == 0:
+        return True
+    else:
+        return False
+
+sum = 0
+
+for i in range(1,100):
+    if is_even(i):
+        sum += i
+        
+print(f"The sum equals to {sum}")
