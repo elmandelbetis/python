5b01dcf7a21751915d1def88a487c6aea0b6d14a
Álvaro Maldonado Medina <126078785+elmandelbetis@users.noreply.github.com>
2025-07-03 17:13:30 +0200

    Create tariffs.py

---
+++ tariffs.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Name:tariffs.py
+
+Un estudiante llevó a cabo un programa que calculó el coste de
+envío de un minorista online para el cliente. El programa basaría el
+coste de envío en el total del carrito y el país de residencia del cliente en
+cuestión.
+
+La tabla de abajo muestra los detalles de los gastos de envío:
+Pais         Total      Precio de envio
+ USA         <$50           Free
+           $50 - $99         $10
+          $100 - $249        $25
+            >$250            $50
+ 
+ AU          <$50            $10
+          $50 - $99          $20
+          $100-$249          $50
+            >$250           $100
+          
+ CA          <$50            $5
+          $50 -$99           $15
+          $100 - $249        $30
+             >$250           $75
+
+UK           <$50            $20
+          $50 - $99          $25
+          $100 - $249        $55
+             >$250           $110
+ 
+"""
+
