f40a2aff9ec81af58b6e67f2bc74d134cb08292d
Álvaro Maldonado Medina <126078785+elmandelbetis@users.noreply.github.com>
2025-07-22 02:09:36 +0200

    man, what a tough month it's being
    a lot of shitty stuff happening rn at my place, but we stay strong at least

--- zodiac_sign.py
+++ zodiac_sign.py
@@ -6,3 +6,11 @@ Crear un programa Python que permita al usuario conocer su signo astrológico
 desde la fecha de nacimiento dada
 """
 
+def introducir_fecha():
+    fecha = input("Introduce tu fecha de nacimiento (DD/MM/AAAA): ")
+    dia_str, mes_str, anio_str = fecha.split('/')
+    dia, mes, anio = int(dia_str), int(mes_str), int(anio_str)
+
+
+def fecha_correcta(dd, mm, aaaa):
+    
