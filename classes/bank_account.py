8faf6c353a107668d049bfdb37b4e2ca3a1fde41
Álvaro Maldonado Medina <126078785+elmandelbetis@users.noreply.github.com>
2025-07-25 17:00:55 +0200

    Update bank_account.py

--- classes/bank_account.py
+++ classes/bank_account.py
@@ -11,3 +11,28 @@ Métodos: ingreso, reintegro, consulta de saldo, y transferencia a otra cuenta.
 
 """
 
+class CuentaBancaria:
+    numero = ""
+    titular = ""
+    saldo = 0.0
+    
+    def __init__(self, numero, titular, saldo):
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        
+    def __str__(self):
+        return f"Cuenta con número que acaba en: *{self.numero[-3:]}\n \
+            Titular: {self.titular}\nSaldo: {self.saldo} €"
+            
+    def ingresar(self, cantidad):
+        self.saldo += cantidad
+        
+    def reintegro(self, cantidad):
+        self.saldo -= cantidad
+    
+    def consulta_saldo(self):
+        return self.saldo
+    
+    def transferir(self, cuenta2):
+        
