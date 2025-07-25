1a118e52c4eec862ea773712088a6c0f9af8036a
Álvaro Maldonado Medina <126078785+elmandelbetis@users.noreply.github.com>
2025-07-25 14:57:21 +0200

    Create bank_account.py

---
+++ classes/bank_account.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+name: bank_account.py
+
+Diseña una clase CuentaBancaria que maneje los atributos:
+
+    - Titular (nombre)
+    - Saldo
+    
+Métodos: ingreso, reintegro, consulta de saldo, y transferencia a otra cuenta.
+
+"""
+
